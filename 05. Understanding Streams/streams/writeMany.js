d9e363f83a2596e743e8f6ef9164e908f71671ad
Attila Horvath <sweeptic@gmail.com>
2025-03-27 16:24:04 +0100

    6. Fixing the Memory Issue in Our Program

--- 05. Understanding Streams/streams/writeMany.js
+++ 05. Understanding Streams/streams/writeMany.js
@@ -50,22 +50,123 @@
 //   });
 // })();
 
+// const fs = require('fs/promises');
+// // writeMany: 322.561ms !!!!!!!!!!!!!!!!!!!!!!!
+// // DO  NOT DO THIS WAY
+// (async () => {
+//   const commandFileHandler = await fs.open('./test.txt', 'w');
+//   const createFile = async () => {
+//     console.time('writeMany');
+//     const stream = commandFileHandler.createWriteStream();
+//     for (let index = 0; index < 1000000; index++) {
+//       const buff = Buffer.from(` ${index}`, 'utf-8');
+//       stream.write(buff);
+//     }
+//     console.timeEnd('writeMany');
+//   };
+//   commandFileHandler.on('write', async () => {
+//     createFile();
+//   });
+//   commandFileHandler.emit('write');
+// })();
+
+// USING STREAM
+
+// const fs = require('fs/promises');
+
+//   console.time('writeMany');
+// const filehandle = await fs.open('test.txt', 'w');
+
+// const stream = filehandle.createWriteStream();
+
+// 16384 Byte
+// 16 KByte
+// 0.016 MB
+
+//   console.log('writableHighWaterMark: ', stream.writableHighWaterMark);
+
+//   const buff = Buffer.alloc(65535, 10);
+
+//   console.log(buff);
+
+//   const size = stream.write(buff);
+//   console.log(stream.write(buff));
+//   console.log(stream.write(Buffer.alloc(1, 'a')));
+//   console.log(stream.write(Buffer.alloc(1, 'a')));
+//   //   console.log(stream.write(Buffer.alloc(1, 'a')));
+//   //   stream.write(Buffer.alloc(67541, 'a'));
+//   //   console.log(stream.write(Buffer.alloc(67541, 'a')));
+//   console.log(stream.write(Buffer.alloc(1, 'a')));
+//   console.log(stream.write(Buffer.alloc(1, 'a')));
+//   //   console.log(stream.write(Buffer.alloc(65541, 'a')));
+//   console.log(stream.write(Buffer.alloc(1, 'a')));
+//   console.log(stream.write(Buffer.alloc(1, 'a')));
+
+//   console.log('writableLength: ', stream.writableLength);
+
+//   stream.on('drain', () => {
+//     console.log(stream.write(Buffer.alloc(1, 'a')));
+//     // console.log(stream.write(Buffer.alloc(65541, 'a'))); INFINITE LOOP
+//     // console.log(stream.write(Buffer.alloc(1, 'a')));
+//     // console.log(stream.write(Buffer.alloc(1, 'a')));
+//     // console.log(stream.write(Buffer.alloc(1, 'a')));
+
+//     console.log('writableLength: ', stream.writableLength);
+//     console.log('we are now safe to write more!');
+//   });
+
+//   setInterval(() => {}, 1000);
+
+//   filehandle.close();
+
+//   stream.write(buff);
+
+//   console.log('buff', buff);
+//   stream.write(buff);
+//   stream.write(buff);
+//   stream.write(buff);
+//   stream.write(buff);
+//   stream.write(buff);
+
+//   console.log('writableLength: ', stream.writableLength);
+
 const fs = require('fs/promises');
-// writeMany: 322.561ms !!!!!!!!!!!!!!!!!!!!!!!
-// DO  NOT DO THIS WAY
+
 (async () => {
-  const commandFileHandler = await fs.open('./test.txt', 'w');
-  const createFile = async () => {
-    console.time('writeMany');
-    const stream = commandFileHandler.createWriteStream();
-    for (let index = 0; index < 1000000; index++) {
+  const filehandle = await fs.open('test.txt', 'w');
+  const stream = filehandle.createWriteStream();
+
+  let index = 0;
+  let drainedNum = 0;
+
+  function writeMany() {
+    for (index; index < 1000000; index++) {
       const buff = Buffer.from(` ${index}`, 'utf-8');
-      stream.write(buff);
+      const size = stream.write(buff);
+
+      if (index === 999999) {
+        console.log('drainedNum: ', drainedNum);
+        stream.end(buff);
+        // stream.write('abc');  ERROR
+      }
+
+      if (!size) {
+        console.log('Drain at: ', index);
+        break;
+      }
     }
-    console.timeEnd('writeMany');
-  };
-  commandFileHandler.on('write', async () => {
-    createFile();
+  }
+
+  writeMany();
+
+  stream.on('drain', () => {
+    // console.log('DRAIN DONE');
+    // console.log('writableLength: ', stream.writableLength);
+    drainedNum++;
+    writeMany();
   });
-  commandFileHandler.emit('write');
+
+  stream.on('finish', () => {
+    console.log('FINISH');
+  });
 })();
