7ea60a45fe1f72c8881e1f259070bff38f0a56a4
Attila Horvath <sweeptic@gmail.com>
2025-03-27 12:08:15 +0100

    3. Using Streams Naively in Our Solution

--- 05. Understanding Streams/streams/writeMany.js
+++ 05. Understanding Streams/streams/writeMany.js
@@ -1,6 +1,6 @@
+// WRITE
 // // writeMany: 25.919s
 // const fs = require('fs/promises');
-
 // (async () => {
 //   const commandFileHandler = await fs.open('./test.txt', 'w');
 //   const createFile = async (path) => {
@@ -17,25 +17,55 @@
 //   });
 //   commandFileHandler.emit('write');
 // })();
-//
-//
-//
 
 // callback version
 // writeMany: 3.828s sync
 // writeMany: 2.672s
-// writeMany: 4.379s sync with buff
-// writeMany: 2.740s sync with buff
+// WRITESYNC
+// const fs = require('fs');
+// (() => {
+//   console.time('writeMany');
+//   fs.open('./test.txt', 'w', (err, fd) => {
+//     for (let index = 0; index < 1000000; index++) {
+//       const buff = Buffer.from(` ${index}`, 'utf-8');
+//       fs.writeSync(fd, buff);
+//     }
+//     console.timeEnd('writeMany');
+//   });
+// })();
 
-const fs = require('fs');
+// WRITE
+// callback version
+// writeMany: 3.828s sync
+// writeMany: 2.672s
+// const fs = require('fs');
+// (() => {
+//   console.time('writeMany');
+//   fs.open('./test.txt', 'w', (err, fd) => {
+//     for (let index = 0; index < 1000000; index++) {
+//       const buff = Buffer.from(` ${index}`, 'utf-8');
+//       fs.write(fd, buff, () => {});
+//     }
+//     console.timeEnd('writeMany');
+//   });
+// })();
 
-(() => {
-  console.time('writeMany');
-  fs.open('./test.txt', 'w', (err, fd) => {
+const fs = require('fs/promises');
+// writeMany: 322.561ms !!!!!!!!!!!!!!!!!!!!!!!
+// DO  NOT DO THIS WAY
+(async () => {
+  const commandFileHandler = await fs.open('./test.txt', 'w');
+  const createFile = async () => {
+    console.time('writeMany');
+    const stream = commandFileHandler.createWriteStream();
     for (let index = 0; index < 1000000; index++) {
       const buff = Buffer.from(` ${index}`, 'utf-8');
-      fs.write(fd, buff, () => {});
+      stream.write(buff);
     }
     console.timeEnd('writeMany');
+  };
+  commandFileHandler.on('write', async () => {
+    createFile();
   });
+  commandFileHandler.emit('write');
 })();
